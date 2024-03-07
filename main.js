8bb8e1f5fab9b8a7c14134576894b9c9458c78e0
Rayankhanzada <161487567+Rayankhanzada@users.noreply.github.com>
2024-03-07 09:00:20 +0500

    Task no.2

---
+++ main.js
@@ -0,0 +1,2 @@
+var bobby = ("rayan");
+console.log("hello ".concat(bobby, " would you like to learn some Python today?\u201D"));
---
+++ main.ts
@@ -0,0 +1,3 @@
+let bobby = ("rayan");
+
+console.log(`hello ${bobby} would you like to learn some Python today?”`);
